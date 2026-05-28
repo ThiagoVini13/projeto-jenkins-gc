e08cdf21f13db78de759e3b839800c3bd41b2c50
Thiago Vinicius Costa Guimaraes <thiago7313vini@gmail.com>
2026-05-27 22:40:50 -0400

    fix syntax

--- src/temperatureConverter.ts
+++ src/temperatureConverter.ts
@@ -1,7 +1,7 @@
 export class TemperatureConverter {
 
     fahrenheitToCelsius(fahrenheit: number): number {
-        return (fahrenheit - 32 * 5 / 9;
+        return (fahrenheit - 32) * 5 / 9;
     }
 
     celsiusToFahrenheit(celsius: number): number {
